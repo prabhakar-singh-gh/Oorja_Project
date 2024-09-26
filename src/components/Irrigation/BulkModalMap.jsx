2be1cb4516427a476586a51df8afde61c75e7522
Prabhakar Singh <prabhakar9169@gmail.com>
2024-09-26 17:29:25 +0530

    add green marker in map

--- src/components/Irrigation/BulkModalMap.jsx
+++ src/components/Irrigation/BulkModalMap.jsx
@@ -52,7 +52,14 @@ console.log(center , "LAt lOg" , Radius);
   }, [location])
 
 
-
+  const greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
 
 
   const handleMapClick = (e) => {
@@ -173,7 +180,7 @@ console.log(center , "LAt lOg" , Radius);
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; OpenStreetMap contributors'
           />
-          <Marker position={center}  />
+          <Marker position={center}  icon={greenIcon} />
           <Circle center={center} radius={Radius} color="transparent" fillColor="#4493A2" fillOpacity={0.3} />
         </MapContainer>
 
